bfe5ad7a446ebfa47bd37a1d03713ee1bf5c6555
jsw <develjsw@gmail.com>
2024-05-26 22:10:52 +0900

    api service 추가

--- src/goods/goods.module.ts
+++ src/goods/goods.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { GoodsController } from './goods.controller';
 import { GoodsRepository } from './repositories/goods.repository';
 import { GoodsService } from './servicies/goods.service';
+import { ApiService } from '../api/api.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
     exports: [],
-    imports: [],
-    providers: [GoodsService, GoodsRepository],
+    imports: [HttpModule],
+    providers: [GoodsService, ApiService, GoodsRepository],
     controllers: [GoodsController]
 })
 export class GoodsModule {}
