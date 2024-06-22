938017d3675c6e578e97069e9e53f86ded513ac5
jsw <develjsw@gmail.com>
2024-06-22 22:25:42 +0900

    미사용 slack 모듈 제거

--- src/common/webhook/slack/slack.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SlackService } from './slack.service';
-
-@Module({
-    imports: [],
-    providers: [SlackService],
-    exports: []
-})
-export class SlackModule {}
